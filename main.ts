8395ab8e4a4a789e898ee030a6f173048468d871
angelo-yalung <144264086+angelo-yalung@users.noreply.github.com>
2023-09-15 12:46:46 -0400

    Update pxt.json, main.ts, README.md

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
  * Created by: Angelo Yalung
- * Created on: Sep 2020
- * This program is meant to write "Hello World!" on to the microbit
+ * Created on: Sep 2023
+ * This program is meant to write "Hello, MTHS!" on to the microbit
 */
 
-basic.showString('Hello, World!')
+basic.showString('Hello, MTHS!')
